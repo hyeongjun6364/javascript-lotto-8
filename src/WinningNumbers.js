e929dbb5f6c6fb11cd4b052f7baf22f3adeee3ef
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-31 18:56:51 +0900

    feat: 당첨번호, 보너스번호 관리, 입력값 파싱 구현

---
+++ src/WinningNumbers.js
@@ -0,0 +1,22 @@
+class WinningNumbers {
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor(numbers, bonusNumber) {
+    this.#winningNumbers = this.parseWinningNumbers(numbers);
+    this.#bonusNumber = bonusNumber;
+  }
+
+  parseWinningNumbers(numbers) {
+    return numbers.split(',').map((num) => Number(num.trim()));
+  }
+
+  getNumbers() {
+    return this.#winningNumbers;
+  }
+
+  getBonusNumber() {
+    return this.#bonusNumber;
+  }
+}
+export default WinningNumbers;
