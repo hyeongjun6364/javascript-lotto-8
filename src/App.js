947454bde84dd370d7922e6a27a565519baea886
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-01 03:20:04 +0900

    feat: 구입금액 유효성 검사 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { INPUT_MESSAGES, OUTPUT_MESSAGES } from './constants.js';
 import InputView from './inputView.js';
 import LottoService from './model/LottoService.js';
-import LottoStatistics from './model/LottoResult.js';
+import LottoResult from './model/LottoResult.js';
 import OutputView from './outputView.js';
 import WinningNumbers from './model/WinningNumbers.js';
 
 class App {
   async run() {
-    const money = await InputView.userInput(INPUT_MESSAGES.purchaseInput);
+    const money = await InputView.requestMoneyInput();
     const lottoCount = LottoService.countLottos(money);
     OutputView.printMessage(OUTPUT_MESSAGES.purchaseLottoCount(lottoCount));
 
@@ -19,7 +19,7 @@ class App {
     const bonusNumberInput = await InputView.userInput(INPUT_MESSAGES.bonusNumberInput);
     const winningNumbers = new WinningNumbers(winningNumbersInput, bonusNumberInput);
 
-    const statistics = new LottoStatistics(lottos, winningNumbers);
+    const statistics = new LottoResult(lottos, winningNumbers);
     statistics.calculateRank();
     OutputView.printWinningResult(statistics.getRankCounts());
 
--- src/inputView.js
+++ src/inputView.js
@@ -1,7 +1,34 @@
 import { Console } from '@woowacourse/mission-utils';
+import { ERROR_MESSAGES, INPUT_MESSAGES } from './constants.js';
 class InputView {
   static async userInput(message) {
     return await Console.readLineAsync(message);
+  }
+
+  static async requestMoneyInput() {
+    try {
+      const money = await this.userInput(INPUT_MESSAGES.purchaseInput);
+      this.validateMoneyInput(money);
+      return money;
+    } catch (error) {
+      Console.print(error.message);
+      return this.requestMoneyInput();
+    }
+  }
+
+  static validateMoneyInput(money) {
+    if (Number.isNaN(Number(money))) {
+      throw new Error(ERROR_MESSAGES.invalidMoneyNumber);
+    }
+    if (money % 1000 !== 0) {
+      throw new Error(ERROR_MESSAGES.invalidMoneyUnit);
+    }
+    if (money.length === 0) {
+      throw new Error(ERROR_MESSAGES.emptyMoneyInput);
+    }
+    if (money.trim() === '') {
+      throw new Error(ERROR_MESSAGES.blankMoneyInput);
+    }
   }
 }
 
