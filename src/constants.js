3fafe757295ddc86dbe6d32ef964b3cc73da48da
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-01 04:23:40 +0900

    feat: 보너스번호, 로또번호 에러메세지 상수화

--- src/constants.js
+++ src/constants.js
@@ -37,4 +37,15 @@ export const ERROR_MESSAGES = Object.freeze({
     notEmpty: `${ERROR_PREFIX} 당첨 번호는 빈 문자열일 수 없습니다.`,
     invalidInput: `${ERROR_PREFIX} 당첨 번호는 숫자와 쉼표만 입력해야 합니다.`,
   },
+  bonusNumber: {
+    notNumber: `${ERROR_PREFIX} 보너스 번호는 숫자여야 합니다.`,
+    invalidRange: `${ERROR_PREFIX} 보너스 번호는 1부터 45 사이의 숫자여야 합니다.`,
+    notEmpty: `${ERROR_PREFIX} 보너스 번호는 빈 문자열일 수 없습니다.`,
+    notDuplicated: `${ERROR_PREFIX} 보너스 번호는 당첨 번호와 중복될 수 없습니다.`,
+  },
+  lotto: {
+    notDuplicate: `${ERROR_PREFIX} 로또 번호에 중복된 값이 있으면 안됩니다.`,
+    notInRange: `${ERROR_PREFIX} 로또 번호에 범위는 1~45이어야 합니다.`,
+    notLength6: `${ERROR_PREFIX} 로또 번호는 6개여야 합니다.`,
+  },
 });
