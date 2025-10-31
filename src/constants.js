49f5f3d8f18fc4f9ff433dad8f5ff5a441fff15d
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-10-31 18:56:19 +0900

    refactor: 당첨번호, 보너스번호 \n prefix 추가

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const INPUT_MESSAGES = Object.freeze({
   purchaseInput: '구입금액을 입력해 주세요.\n',
-  winningNumbersInput: '당첨 번호를 입력해 주세요.\n',
-  bonusNumberInput: '보너스 번호를 입력해 주세요.\n',
+  winningNumbersInput: '\n당첨 번호를 입력해 주세요.\n',
+  bonusNumberInput: '\n보너스 번호를 입력해 주세요.\n',
 });
 
 export const OUTPUT_MESSAGES = Object.freeze({
