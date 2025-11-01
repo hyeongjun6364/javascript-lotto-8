e9fe6f77aa74892dbde7d87f9afdcedeaee0041f
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-02 01:25:02 +0900

    test: 로또 유효성 테스트

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,18 +1,27 @@
-import Lotto from "../src/Lotto";
+import Lotto from '../src/model/Lotto';
 
-describe("로또 클래스 테스트", () => {
-  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+describe('로또 클래스 테스트', () => {
+  test('로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 6, 7]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
   // TODO: 테스트가 통과하도록 프로덕션 코드 구현
-  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+  test('로또 번호에 중복된 숫자가 있으면 예외가 발생한다.', () => {
     expect(() => {
       new Lotto([1, 2, 3, 4, 5, 5]);
-    }).toThrow("[ERROR]");
+    }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test('로또 번호가 1~45 범위를 벗어나면 예외가 발생한다.', () => {
+    expect(() => {
+      new Lotto([0, 2, 3, 4, 5, 6]);
+    }).toThrow('[ERROR]');
+  });
+
+  test('올바른 로또 번호가 주어지면 정상적으로 생성된다.', () => {
+    const lotto = new Lotto([3, 11, 15, 29, 35, 44]);
+    expect(lotto.getNumbers()).toEqual([3, 11, 15, 29, 35, 44]);
+  });
 });
